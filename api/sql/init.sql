a3ae5fd3508668b3132c3ee263338cd57f9a7233
metwse <108795071+metwse@users.noreply.github.com>
2023-02-19 00:54:37 +0300

    1.0.4

--- api/sql/init.sql
+++ api/sql/init.sql
@@ -1,3 +1,33 @@
+-- DROP SCHEMA IF EXISTS exams;
+CREATE SCHEMA IF NOT EXISTS exams AUTHORIZATION postgres;
+-- DROP SCHEMA IF EXISTS lessons;
+CREATE SCHEMA IF NOT EXISTS lessons AUTHORIZATION postgres;
+-- DROP SCHEMA IF EXISTS publishers;
+CREATE SCHEMA IF NOT EXISTS publishers AUTHORIZATION postgres;
+
+
+
+-- DROP TABLE IF EXISTS public.publishers;
+CREATE TABLE IF NOT EXISTS public.publishers (
+    id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
+    name text COLLATE pg_catalog."default" NOT NULL,
+    CONSTRAINT publishers_pkey PRIMARY KEY (id)
+) TABLESPACE pg_default;
+ALTER TABLE IF EXISTS public.publishers OWNER to postgres;
+
+
+
+-- DROP TABLE IF EXISTS public.lessons;
+CREATE TABLE IF NOT EXISTS public.lessons
+(
+    id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
+    name text COLLATE pg_catalog."default" NOT NULL,
+    CONSTRAINT lessons_pkey PRIMARY KEY (id)
+) TABLESPACE pg_default;
+ALTER TABLE IF EXISTS public.lessons OWNER to postgres;
+
+
+
 -- DROP TABLE IF EXISTS public.books;
 CREATE TABLE IF NOT EXISTS public.books (
     id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
@@ -18,13 +48,11 @@ CREATE TABLE IF NOT EXISTS public.books (
         NOT VALID
 ) TABLESPACE pg_default;
 ALTER TABLE IF EXISTS public.books OWNER to postgres;
-
 -- DROP INDEX IF EXISTS public.books_lesson_id;
 CREATE INDEX IF NOT EXISTS books_lesson_id
     ON public.books USING btree
     (lesson_id ASC NULLS LAST)
     TABLESPACE pg_default;
-
 -- DROP INDEX IF EXISTS public.books_publisher_id;
 CREATE INDEX IF NOT EXISTS books_publisher_id
     ON public.books USING btree
@@ -57,45 +85,34 @@ CREATE TABLE IF NOT EXISTS public.exams (
         NOT VALID
 ) TABLESPACE pg_default;
 ALTER TABLE IF EXISTS public.exams OWNER to postgres;
--- Index: exams_index
-
 -- DROP INDEX IF EXISTS public.exams_index;
 CREATE INDEX IF NOT EXISTS exams_index
     ON public.exams USING gist
     (full_name COLLATE pg_catalog."default" gist_trgm_ops)
     INCLUDE(id)
     TABLESPACE pg_default;
-
 -- DROP INDEX IF EXISTS public.exams_category_id;
 CREATE INDEX IF NOT EXISTS exams_category_id
     ON public.exams USING btree
     (category_id ASC NULLS LAST)
     TABLESPACE pg_default;
-
 -- DROP INDEX IF EXISTS public.exams_publisher_id;
 CREATE INDEX IF NOT EXISTS exams_publisher_id
     ON public.exams USING btree
     (publisher_id ASC NULLS LAST)
     TABLESPACE pg_default;
+    
 
 
--- DROP TABLE IF EXISTS public.lessons;
-CREATE TABLE IF NOT EXISTS public.lessons
-(
+-- DROP TABLE IF EXISTS exams.categories;
+CREATE TABLE IF NOT EXISTS exams.categories (
     id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
     name text COLLATE pg_catalog."default" NOT NULL,
-    CONSTRAINT lessons_pkey PRIMARY KEY (id)
+    question_count integer NOT NULL,
+    schema integer[] NOT NULL,
+    CONSTRAINT categories_pkey PRIMARY KEY (id)
 ) TABLESPACE pg_default;
-ALTER TABLE IF EXISTS public.lessons OWNER to postgres;
-
-
--- DROP TABLE IF EXISTS public.publishers;
-CREATE TABLE IF NOT EXISTS public.publishers (
-    id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
-    name text COLLATE pg_catalog."default" NOT NULL,
-    CONSTRAINT publishers_pkey PRIMARY KEY (id)
-) TABLESPACE pg_default;
-ALTER TABLE IF EXISTS public.publishers OWNER to postgres;
+ALTER TABLE IF EXISTS exams.categories OWNER to postgres;
 
 
 -- DROP TABLE IF EXISTS public.questions;
@@ -105,13 +122,3 @@ CREATE TABLE IF NOT EXISTS public.questions (
     count integer NOT NULL
 ) TABLESPACE pg_default;
 ALTER TABLE IF EXISTS public.questions OWNER to postgres;
-
-
--- DROP SCHEMA IF EXISTS exams;
-CREATE SCHEMA IF NOT EXISTS exams AUTHORIZATION postgres;
-
--- DROP SCHEMA IF EXISTS lessons;
-CREATE SCHEMA IF NOT EXISTS lessons AUTHORIZATION postgres;
-
--- DROP SCHEMA IF EXISTS publishers;
-CREATE SCHEMA IF NOT EXISTS publishers AUTHORIZATION postgres;
